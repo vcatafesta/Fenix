e15e18c7d5d60016e6f50da35cee35890cd959f6
vcatafesta <vcatafesta@gmail.com>
2020-05-29 15:53:32 -0400

    upgrade

--- doc/postar.bat
+++ doc/postar.bat
@@ -1,5 +1,8 @@
-git add *.ch
-git add *.prg
-git add *.exe
-git commit -m "atualizacao"
+git add doc/*
+git add include/*.ch
+git add src/*.prg
+git add src/*.c
+git add fenix.exe
+git add fenix.hbp
+git commit -m "upgrade"
 git push
