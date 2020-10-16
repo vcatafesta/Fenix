05454f18539d81af4cf558a1050b5ae7025f0617
vcatafesta <vcatafesta@gmail.com>
2020-10-16 09:41:14 -0400

    atualizacao

--- funcoes/sciapilinux.c
+++ funcoes/sciapilinux.c
@@ -8,6 +8,7 @@
 //#warning "this header is deprecated"
 #endif
 
+
 // synonymn for MS_* 
 	HB_FUNC_TRANSLATE(TRIM,    	RTRIM)
 	HB_FUNC_TRANSLATE(MS_TRIM,    RTRIM)
@@ -16,29 +17,30 @@
 	HB_FUNC_TRANSLATE(MS_LEN,     LEN)
 	HB_FUNC_TRANSLATE(MS_CAPITALIZE, CAPITALIZE)
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
 /*
 static size_t strlen( char s[] )
 {
 	int i = 0;
-	while(s[i] != '\0' )
-	{
+	while(s[i] != '\0' ){
 		i++;
 	}
 	return i;
 }
+
 */
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
-char *chr(MS_SIZE n){
-	char *ch = (char *)malloc(sizeof(char*));
-	ch[1]       = '\0';
-	memset(ch, n, 1);	   
+char *chr( MS_SIZE n )
+{
+   char *ch = (char *)malloc(sizeof(char*));
+   ch[1]    = '\0';
+   memset(ch, n, 1);
    return(ch);
 }
- 
+
 /*-----------------------------------------------------------------------------------------------*/	
 
 void *malloc_s(size_t size) {
@@ -79,7 +81,8 @@ char *space(int x, char ch)
 
 //=================================================================
 
-void *spaceset(size_t size, char ch ) {
+void *spaceset(size_t size, char ch)
+{
     return(memset((char *)malloc_s(size * sizeof(char *)), ch, size));
 }
 
@@ -208,7 +211,7 @@ char *strsubstr(TString str, size_t ini, size_t fim) {
     return (NULL);
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
 static char *replicate(MS_CHAR *str, MS_SIZE vezes)
 {
@@ -217,7 +220,7 @@ static char *replicate(MS_CHAR *str, MS_SIZE vezes)
 	MS_CHAR *ptr   = (MS_CHAR*)malloc(tam * sizeof(MS_CHAR)); // (MS_CHAR*)malloc(tam+1);
 	MS_SIZE x;
 	MS_SIZE y;
-		
+
 	if (str){
 		if (ptr){
 			for (x = 0; x < tam;){
@@ -230,10 +233,10 @@ static char *replicate(MS_CHAR *str, MS_SIZE vezes)
 	ptr[vezes] = '\0';
 	if(ptr)
 		return ptr;
-	return NULL;		
+	return NULL;
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
  
 HB_FUNC( MS_CLS )
 {
@@ -330,23 +333,27 @@ HB_FUNC( MS_CLS )
    return;
 }	
 
-/*-----------------------------------------------------------------------------------------------*/	
-
+/*-----------------------------------------------------------------------------------------------
+ms_replicate(<string>, <nvezes>)
+ms_replicate("=", 80)
+-----------------------------------------------------------------------------------------------*/
 HB_FUNC( MS_REPLICATE )
 {
 	int iParams = hb_pcount();
-	
-	if( iParams == 2 && HB_ISCHAR( 1 ) && HB_ISNUM( 2 ) )
+
+//	if( iParams == 2 && HB_ISCHAR( 1 ) && HB_ISNUM( 2 ) )
+	if(hb_pcount() == 2 && HB_ISCHAR(1) && HB_ISNUM(2))
    {
-		char *szText = replicate( (char*)hb_parc( 1 ), hb_parni( 2 ) );
-		hb_retc( szText );
+		char *szText = replicate((char*)hb_parc(1), hb_parni(2));
+		hb_retc(szText);
+		free(szText);
 		return;
 	}
 	else
-   {	
+   {
 		hb_errRT_BASE_SubstR( EG_ARG, 1111, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
 		return;
-	}	
+	}
 }
 
 /*-----------------------------------------------------------------------------------------------*/	
@@ -385,20 +392,23 @@ HB_FUNC( MS_FOR )
 	return;
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
-
-HB_FUNC(MS_CLEAR){
-	MS_CHAR	*string 	= (char*) hb_parc(2);	
+/*-----------------------------------------------------------------------------------------------
+ms_clear(<color>, <string>)
+ms_clear(31, "░▒▓")
+-----------------------------------------------------------------------------------------------*/
+HB_FUNC(MS_CLEAR)
+{
+	MS_CHAR	*string 	= (char*) hb_parc(2);
 	MS_INT	x 			= strlen(string);
 	MS_INT	iTop    	= 0;
 	MS_INT 	iLeft   	= 0;
 	MS_INT 	iBottom 	= hb_gtMaxRow();
-	MS_INT	iRight  	= hb_gtMaxCol();	
+	MS_INT	iRight  	= hb_gtMaxCol();
 	MS_INT	size 		= (MS_INT)(((iBottom-iTop)) * ((iRight-iLeft)));
-	MS_CHAR	*buffer 	= (MS_CHAR*)malloc(size);	
-	MS_INT	n;	
+	MS_CHAR	*buffer 	= (MS_CHAR*)malloc(size);
+	MS_INT	n;
 	MS_INT 	y;
-		
+
 	for (n=0; n<size;){
 		for (y=0; y<x; y++, n++){
 			buffer[n] = string[y];
@@ -1112,38 +1122,27 @@ static void hb_inkeySetTextKeys( const char * pszText, HB_SIZE nSize, HB_BOOL fI
    }
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
 HB_FUNC( INKEY )
 {
    int iPCount = hb_pcount();
 
-   hb_retni( 
+   hb_retni(
 					hb_inkey( iPCount == 1 || ( iPCount > 1 && HB_ISNUM( 1 )),
-               hb_parnd( 1 ), 
+               hb_parnd( 1 ),
 					hb_parnidef( 2, hb_setGetEventMask()))
 				);
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
-
-HB_FUNC( __KEYBOARD )
-{
-   /* Clear the typeahead buffer without reallocating the keyboard buffer */
-   hb_inkeyReset();
-
-   if( HB_ISCHAR( 1 ) )
-      hb_inkeySetText( hb_parc( 1 ), hb_parclen( 1 ) );
-}
-
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
 HB_FUNC( HB_KEYCLEAR )
 {
    hb_inkeyReset();
 }
 
-/*-----------------------------------------------------------------------------------------------*/	
+/*-----------------------------------------------------------------------------------------------*/
 
 HB_FUNC( HB_KEYPUT )
 {
@@ -2886,7 +2885,7 @@ char * hb_verCompiler( void )
    #elif defined( __EMX__ )
       pszName = "EMX GNU C";
    #else
-      pszName = "GNU C";
+      pszName = "GCC/GNU C";
    #endif
 
    #if defined( __cplusplus )
@@ -2975,6 +2974,50 @@ char * hb_verCompiler( void )
    return pszCompiler;
 }
 
+/* NOTE: The caller must free the returned buffer. [vszakats] */
+
+/* NOTE:
+   CA-Cl*pper 5.2e returns: "Clipper (R) 5.2e Intl. (x216)  (1995.02.07)"
+   CA-Cl*pper 5.3b returns: "Clipper (R) 5.3b Intl. (Rev. 338) (1997.04.25)"
+ */
+
+/*
+char * hb_verHarbour( void )
+{
+   char * pszVersion;
+   char szDateRaw[ 11 ];
+   char szDate[ 17 ];
+
+   HB_TRACE( HB_TR_DEBUG, ( "hb_verHarbour()" ) );
+
+   hb_snprintf( szDateRaw, sizeof( szDateRaw ), "%d", hb_verCommitRev() );
+
+   szDate[ 0 ] = '2';
+   szDate[ 1 ] = '0';
+   szDate[ 2 ] = szDateRaw[ 0 ];
+   szDate[ 3 ] = szDateRaw[ 1 ];
+   szDate[ 4 ] = '-';
+   szDate[ 5 ] = szDateRaw[ 2 ];
+   szDate[ 6 ] = szDateRaw[ 3 ];
+   szDate[ 7 ] = '-';
+   szDate[ 8 ] = szDateRaw[ 4 ];
+   szDate[ 9 ] = szDateRaw[ 5 ];
+   szDate[ 10 ] = ' ';
+   szDate[ 11 ] = szDateRaw[ 6 ];
+   szDate[ 12 ] = szDateRaw[ 7 ];
+   szDate[ 13 ] = ':';
+   szDate[ 14 ] = szDateRaw[ 8 ];
+   szDate[ 15 ] = szDateRaw[ 9 ];
+   szDate[ 16 ] = '\0';
+
+   pszVersion = ( char * ) hb_xgrab( 80 );
+   hb_snprintf( pszVersion, 80, "Harbour %d.%d.%d%s (%s) (%s)",
+                HB_VER_MAJOR, HB_VER_MINOR, HB_VER_RELEASE, HB_VER_STATUS,
+                hb_verCommitIDShort(), szDate );
+
+   return pszVersion;
+}
+*/
 
 char * hb_verPCode( void )
 {
